be648e0054767a0c6a8df4a7d72dee7cde1e7523
Ravi kiran <Ravi kiran@LAPTOP-96V3ECC4>
2024-01-27 11:40:51 +0530

    Added testng annotations

--- src/test/java/Ravikiran/seleniumframeworkDesign/Addcart.java
+++ src/test/java/Ravikiran/seleniumframeworkDesign/Addcart.java
@@ -26,52 +26,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Addcart extends BaseTest {
 
 	@Test
-	
-	public void submitorder() throws IOException, InterruptedException
-	{
-		
+
+	public void submitorder() throws IOException, InterruptedException {
+
 		// TODO Auto-generated method stub
 		String name = "ADIDAS ORIGINAL";
-		String finalconfirmation="THANKYOU FOR THE ORDER.";
-		Homepage home  = LaunchApplication();
-		//Adding comments
+		String finalconfirmation = "THANKYOU FOR THE ORDER.";
 		
-		
-		
-		
-		Products prodc =home.login("test@mailsac.com", "Testuser123@");
-		//Products prodc = new Products(driver);
+		// Adding comments
+
+		Products prodc = home.login("test@mailsac.com", "Testuser123@");
+		// Products prodc = new Products(driver);
 		List<WebElement> product = prodc.getProductslist();
-		
-		
-		//List<WebElement> product = driver.findElements(By.className("mb-3"));
-		 prodc.getProductslist();
+
+		// List<WebElement> product = driver.findElements(By.className("mb-3"));
+		prodc.getProductslist();
 		prodc.getPoductname(name);
-	   // prodc.getProductslist();
+		// prodc.getProductslist();
 		prodc.addtocart(name);
-		cartpage cart =prodc.gotocart();
-		//cartpage cart = new cartpage(driver);
+		cartpage cart = prodc.gotocart();
+		// cartpage cart = new cartpage(driver);
 		boolean val = cart.checkcartitems(name);
 		Assert.assertTrue(val);
-		checkoutpage chck=cart.checkout();
+		checkoutpage chck = cart.checkout();
 		chck.selectcountry("ind");
-		Confirmationpage confirm =chck.confirmationpage();
+		Confirmationpage confirm = chck.confirmationpage();
 		String tect = confirm.confirmationcheck();
 		System.out.print(tect);
 		Assert.assertEquals(finalconfirmation, tect);
-		driver.quit();
-				
-		
-		
-		
-	    
-		
-	    
-	     
-	     
-	    
-	    
-	    
+		//driver.quit();
+
 	}
 
 }
--- src/test/java/Ravikiran/seleniumframeworkDesign/Test/BaseTest.java
+++ src/test/java/Ravikiran/seleniumframeworkDesign/Test/BaseTest.java
@@ -10,52 +10,53 @@ import org.bouncycastle.oer.its.ieee1609dot2.basetypes.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import Ravikiran.seleniumframeworkDesign.PageObjects.Homepage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
-
 public class BaseTest {
-	
+
 	public WebDriver driver;
-	
+	public Homepage home;
+
 	@SuppressWarnings("deprecation")
-	public WebDriver intializeDriver() throws IOException
-	{
-	
-		
-		
+	public WebDriver intializeDriver() throws IOException {
+
 		Properties pro = new Properties();
-		
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//Ravikiran//seleniumframeworkDesign//resources//GlobalData.properties");
+
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+				+ "//src//main//java//Ravikiran//seleniumframeworkDesign//resources//GlobalData.properties");
 		pro.load(fis);
-		
-        String browseri = pro.getProperty("browser");
-        if(browseri.equalsIgnoreCase("Chrome"))
-        		{
-	    WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
-		
-        		}
- driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
+		String browseri = pro.getProperty("browser");
+		if (browseri.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 		driver.manage().window().maximize();
-		
+
 		return driver;
-         
-		
-		
+
 	}
-	
-	public Homepage LaunchApplication() throws IOException
-	{
-	driver = intializeDriver();
-		Homepage home = new Homepage(driver);
+	@BeforeTest
+
+	public Homepage LaunchApplication() throws IOException {
+		driver = intializeDriver();
+		 home = new Homepage(driver);
 		home.seturl("https://rahulshettyacademy.com/client");
 		return home;
 	}
 	
+	@AfterTest
 	
+	public void Closetabs()
+	{
+		driver.close();
+	}
 
 }
